cae254d81c6c238bd2ea5744773779cb69de282a
tuandat732 <spt.may7320@gmail.com>
2020-09-02 16:27:35 +0700

    add check banned word

--- mobile_shop/modules/product/reply_cmt.php
+++ mobile_shop/modules/product/reply_cmt.php
@@ -9,6 +9,9 @@ if (isset($_POST['sbm']) && isset($_GET['comm_id'])) {
   $comm_name = $_POST['rep_name'];
   date_default_timezone_set('asia/Ho_Chi_Minh');
   $comm_date = date("Y-m-d H:i:s");
+
+  include_once("./check_comment.php"); //file xóa từ cấm
+
   $sql = "INSERT INTO comment (rep_comm_id,comm_date,comm_details,comm_mail,comm_name,prd_id) 
     VALUES ($rep_comm_id,'$comm_date','$comm_details','$comm_mail','$comm_name',$prd_id)";
   mysqli_query($conn, $sql);
